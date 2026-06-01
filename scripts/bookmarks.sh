b3ae1ab2f6b5720bdc466ea6468abaa7913a2df8
Nam Nguyen <nguyenvietnam2401@gmail.com>
2026-06-01 12:32:37 +1000

    feat: restructure pages and update bookmark toc

--- scripts/bookmarks.sh
+++ scripts/bookmarks.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-toc_out="    <ul id=\"toc\" class=\"[ toc ]\">\n"
-bm_out="    <article>\n"
+toc_out="      <p id=\"toc\" class=\"[ toc ]\">\n"
+toc_placeholder='<toc-out-placeholder>'
+bm_out="${bm_out}${toc_placeholder}\n"
 last_category=""
 blocks_path="./src/blocks"
 
@@ -20,13 +21,18 @@ while IFS="|" read -r __category __title __url; do
     if [[ -n "${last_category}" ]]; then
       bm_out="${bm_out}      </ul>\n"
       bm_out="${bm_out}      <!-- block-start: back-to-top -->\n"
-      bm_out="${bm_out}      <!-- block-end: back-to-top -->\n"
+      bm_out="${bm_out}      <!-- block-end: back-to-top -->\n\n"
     fi
-
-    toc_out="${toc_out}      <li><a href=\"#${id}\" id=\"toc-${id}\">${category}</a></li>\n"
-
     bm_out="${bm_out}      <h2 id=\"${id}\"><a href=\"#${id}\">${category}</a></h2>\n"
     bm_out="${bm_out}      <ul>\n"
+
+
+    if [[ -n "${last_category}" ]]; then
+        toc_out="${toc_out}        • "
+    else
+        toc_out="${toc_out}        "
+    fi
+    toc_out="${toc_out}<a href=\"#${id}\" id=\"toc-${id}\">${category}</a>\n"
 
     last_category="${category}"
   fi
@@ -34,11 +40,12 @@ while IFS="|" read -r __category __title __url; do
   bm_out="${bm_out}        <li><a href=\"${url}\" target=\"_blank\" data-anchor=\"none\">${title}</a></li>\n"
 done < <(bm view)
 
-toc_out="${toc_out}    </ul>\n"
+toc_out="${toc_out}      </p>\n"
 
 bm_out="${bm_out}      </ul>\n"
 bm_out="${bm_out}      <!-- block-start: back-to-top -->\n"
 bm_out="${bm_out}      <!-- block-end: back-to-top -->\n"
-bm_out="${bm_out}    </article>\n"
 
-printf "%b" "${toc_out}\n${bm_out}" > "${blocks_path}/bookmarks.html"
+bm_out="${bm_out/${toc_placeholder}/${toc_out}}"
+
+printf "%b" "${bm_out}" > "${blocks_path}/bookmarks.html"
